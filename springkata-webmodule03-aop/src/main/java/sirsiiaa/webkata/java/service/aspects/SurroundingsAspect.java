f184a9ffd17f1ac3a24fef588492026068ef32d7
siiaa <themiao@outlook.com>
2025-02-23 11:49:50 +0800

    1. spring aop surroundings aspect

--- springkata-webmodule03-aop/src/main/java/sirsiiaa/webkata/java/service/aspects/SurroundingsAspect.java
+++ springkata-webmodule03-aop/src/main/java/sirsiiaa/webkata/java/service/aspects/SurroundingsAspect.java
@@ -11,12 +11,16 @@ public class SurroundingsAspect {
     /*
      * 1. 环绕增强必须携带返回值，该obj即为环绕方法的返回值
      * 2. 环绕增强使用ProceedingJoinPoint获取原方法上下文信息
-     * 3.
+     * 3. 使用环绕通知需要
      */
     @Around("sirsiiaa.webkata.java.service.aspects.LogAspect.pointcut()")
     public Object commit(ProceedingJoinPoint joinPoint) throws Throwable {
         var args = joinPoint.getArgs();
         Object res = null;
+
+        /*
+         * 在多个增强中需要共享的变量
+         */
         long startTime = System.currentTimeMillis();
         try {
             res = joinPoint.proceed();
