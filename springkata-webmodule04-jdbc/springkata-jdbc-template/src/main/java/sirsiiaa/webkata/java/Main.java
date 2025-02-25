085d995622b537b87ef54a1614da9d0112372777
长生 <themiao@outlook.com>
2025-02-25 23:40:10 +0800

    1. sync spring tx [update]

--- springkata-webmodule04-jdbc/springkata-jdbc-template/src/main/java/sirsiiaa/webkata/java/Main.java
+++ springkata-webmodule04-jdbc/springkata-jdbc-template/src/main/java/sirsiiaa/webkata/java/Main.java
@@ -3,6 +3,15 @@ package sirsiiaa.webkata.java;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    /*
+     * spring-tx 包含了封装好的事务AOP以及TransactionManager接口
+     * spring-jdbc 包含了DataSourceTransactionManager实现类，封装面向Connection的事务操作
+     * spring-orm 包含了HibernateTransactionManager、JPATransactionManager实现类，封装面向Hibernate、JPA的事务操作
+     *
+     * 使用tx的步骤：
+     *  1. 引入所需的事务管理器
+     *  2. 将事务管理器纳入IoC容器，然后配置生效
+     */
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
