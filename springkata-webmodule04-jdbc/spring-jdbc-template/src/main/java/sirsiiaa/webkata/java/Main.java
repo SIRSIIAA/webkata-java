e0cb974a4593218c2d7f5f24d462ff5c764655f1
siiaa <themiao@outlook.com>
2025-02-25 21:11:36 +0800

    1. spring tx

--- springkata-webmodule04-jdbc/spring-jdbc-template/src/main/java/sirsiiaa/webkata/java/Main.java
+++ springkata-webmodule04-jdbc/spring-jdbc-template/src/main/java/sirsiiaa/webkata/java/Main.java
@@ -1,6 +1,14 @@
 package sirsiiaa.webkata.java;
 
 public class Main {
+    /*
+     * spring-tx 包含了封装好的事务AOP以及TransactionManager接口
+     * spring-jdbc 包含了DataSourceTransactionManager实现类，封装面向Connection的事务操作
+     * spring-orm 包含了HibernateTransactionManager、JPATransactionManager实现类，封装面向Hibernate、JPA的事务操作
+     *
+     * 使用tx的步骤：
+     *  1. 引入所需的
+     */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
